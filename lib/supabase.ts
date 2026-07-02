e5476143bbf435a274f51b218d14b72a6c787b86
Blubbaeri <ridhomuhammadghadir@gmail.com>
2026-07-02 19:47:25 +0700

    fix: add fallback url for supabase to prevent blank screen crash on apk startup

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -2,9 +2,8 @@ import 'react-native-url-polyfill/auto';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js';
 
-// TODO: Replace with your Supabase URL and Anon Key
-const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL!
-const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY!
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL || 'https://ajgiotqbihibjsfytkls.supabase.co';
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFqZ2lvdHFiaWhpYmpzZnl0a2xzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE3NDMxMTAsImV4cCI6MjA4NzMxOTExMH0.ZOwaCkCehON_Z8HtwNgj7uxCdjowlNqqOlvBJ79GMtE';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
     auth: {
